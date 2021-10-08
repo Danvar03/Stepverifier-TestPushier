87c8ccfe9b5dce007c1c1ec8104c97b1af307d26
Danvar03 <daniela.03v@gmail.com>
2021-10-07 23:43:35 -0500

    Se reakuzliza TestPublisher

---
+++ src/test/java/com/sofka/StepVerifier/Services/UppercaseConverter.java
@@ -0,0 +1,16 @@
+package com.sofka.StepVerifier.Services;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class UppercaseConverter {
+    private final Flux<String> source;
+    UppercaseConverter(Flux<String> source) {
+        this.source = source;
+    }
+    Flux<String> getUpperCase() {
+        return source.map(String::toUpperCase);
+    }
+}
+
